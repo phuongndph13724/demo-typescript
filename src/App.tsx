69c4d8a2e966726d36109efa4b8ce25a2128e09b
dacphuongotp <dacphuongotp@gmail.com>
2022-03-16 14:42:34 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './components/ShowInfo'
+import ShowInfo from './components/ShowInfo';
+import { list, remove } from './api/product';
 import { ProductType } from './types/product'
 import axios from 'axios'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]);
-  const [count, setCount] = useState<number>(0);
+  // const [count, setCount] = useState<number>(0);
   useEffect(() => {
     const getProducts = async () => {
-      const {data} = await axios.get('http://localhost:3001/products');
+      const {data} = await list();
       setProducts(data);
     }
     getProducts();
   },[])
+  const removeItem = async (id: number) => {
+    // xoa tren API
+    const { data } = await remove(id);
+    // reRender
+    data && setProducts(products.filter(item => item._id !== data._id));
+  }
   return (
     <div className="App">
       <hr></hr>
        <div>
-        <table border="1">
+        {/* <table className='App' border="1">
           <thead>
             <th>Stt</th>
           <th>Name</th>
@@ -31,9 +39,26 @@ function App() {
             <td>{item.name}</td>
             <td>{item.price}</td>
             <td>{item.title}</td>
+            <td>
+              <button onClick={() => removeItem(item._id)}>Remove</button>
+            </td>
           </tbody>)}
           
-        </table>
+        </table> */}
+        <header>
+          <ul>
+            <li><a href="/">Home Page</a></li>
+            <li><a href="/product">Product Page</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </header>
+        <main>
+          <Routes>
+              <Route path="/" element={<h1>Home page</h1>} />
+              <Route path="product" element={<h1>Product page</h1>} />
+              <Route path="/about" element={<h1>About page</h1>} />
+          </Routes>
+        </main>
       </div>
     </div>
   )
